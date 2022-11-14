f169093599ba93c1df1a1c5c96ee0dacc3802a79
Bastien Piguet <golden0kamia@gmail.com>
2022-11-14 23:47:27 +0100

    error files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 function showOnly(n){
     const webSlide = document.getElementById("website");
     const servSlide = document.getElementById("services");
@@ -19,11 +20,33 @@ function showOnly(n){
             servButton.style.backgroundColor="white";
             webButton.style.color="white";
             webButton.style.backgroundColor="rgb(20, 20, 20)";
+
+            let myinfo = document.getElementById("heiaPlayer");
+            myinfo.innerText = "Loading";
+            fetch("https://api.mcsrvstat.us/2/heia.mc.piguetb.ch").then(res => {return res.json()}).then(json =>{
+                let myinfo = document.getElementById("heiaPlayer");
+                let mytext = document.getElementById("heiaMotd");
+                let myIcon = document.getElementById("heiaIcon");
+                myIcon.src = json.icon;
+                mytext.innerHTML = json.motd.html;
+                myinfo.innerText = json.players.online + "/" + json.players.max;
+            });
             break;
         default:
-            webSlide.style.display = "block";
-            servSlide.style.display = "none";
             break;
+    }
+}
+
+function showMore(){
+    let button = document.getElementById("hiddenButton");
+    let moreLink = document.getElementById("privateLink");
+    if(moreLink.style.display == "none"){
+        moreLink.style.display = "block";
+        button.textContent="Less";
+    }
+    else{
+        moreLink.style.display = "none";
+        button.textContent="More";
     }
 }
   
